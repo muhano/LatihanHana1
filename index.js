8c2829fabc69b4468ef96335eae1d918d3ee987e
muhano <geri.muhano@gmail.com>
2022-03-25 15:46:51 +0700

    get access token

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const axios = require('axios').default;
 
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.post('/api/auth', async (req, res) => {
+  try {
+    const getToken = await axios({
+      method: 'post',
+      url: 'http://10.25.88.173:8080/api/auth',
+      auth: {
+        username: process.env.GETUSERNAME,
+        password: process.env.GETPASSWORD
+      },
+      headers: { 'client_id': 'keb_hana' }
+    });
+
+    console.log(getToken);
+    res.status(getToken.status).json(getToken.data)
+  } catch (err) {
+    console.log(err, '<------------------------------');
+    res.status(err.status).json(err.data)
+  }
 })
 
 app.listen(port, () => {
