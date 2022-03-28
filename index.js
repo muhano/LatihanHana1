6bc37e5d22a010f72202abe6add0a29e06e4f48b
muhano <geri.muhano@gmail.com>
2022-03-28 08:37:43 +0700

    add new route for fetch data

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ app.get('/api/v1/data/nik/:NIK', async (req, res) => {
     const getData = await axios({
       method: 'get',
       url: `http://10.25.88.173:8081/api/v1/data/nik/${NIK}`,
-      headers: { 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRlIjoiMjAyMi0wMy0yNSIsInVzZXJJZCI6ImJrZWJoYW5hIiwidGltZSI6IjEwOjEyOjUyIn0.k9IL2He23SfKanWYrpRkkKBi7JP0QVDrs94kLdc7-R8' }
+      headers: { 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRlIjoiMjAyMi0wMy0yOCIsInVzZXJJZCI6ImJrZWJoYW5hIiwidGltZSI6IjAzOjE0OjU0In0.1NFfHDsFaK8jfIiYltYsnbMDKWPc44erBvLBWg5CY7U' }
     });
 
-    console.log(getData, '<-----------------------');
+    // console.log(getData, '<-----------------------');
     res.status(getData.status).json(getData.data)
   } catch (err) {
     //console.log(err, '<+++++++++++++++++');
@@ -49,6 +49,36 @@ app.get('/api/v1/data/nik/:NIK', async (req, res) => {
   }
 })
 
+
+app.get('/api/v2/data/nik/:NIK', async (req, res) => {
+  try {
+    const {NIK} = req.params
+
+    const getToken = await axios({
+      method: 'post',
+      url: 'http://10.25.88.173:8080/api/auth',
+      auth: {
+        username: process.env.GETUSERNAME,
+        password: process.env.GETPASSWORD
+      },
+      headers: { 'client_id': 'keb_hana' }
+    });
+
+    accessToken = getToken.data.access_token
+
+    const getData = await axios({
+      method: 'get',
+      url: `http://10.25.88.173:8081/api/v1/data/nik/${NIK}`,
+      headers: { 'Authorization': `Bearer ${accessToken}` }
+    });
+
+    // console.log(getData, '<-----------------------');
+    res.status(getData.status).json(getData.data)
+  } catch (err) {
+    //console.log(err, '<+++++++++++++++++');
+    res.status(err.response.status).json(err.response.data)
+  }
+})
 
 
 app.listen(port, () => {
